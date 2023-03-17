54df04d4fc44bb61a78fa58143c9edda7da3da83
Heather Lee <leeh1102@gmail.com>
2023-03-16 18:09:23 -0700

    Implement get call

--- server.js
+++ server.js
@@ -9,6 +9,25 @@ const { postSchema, Post } = require("./post");
 const posts = mongoose.model('posts', postSchema);
 app.use(express.json());
 
+// TODO: Error Handling 
+
+// GET /acronym?page=1&limit=10&search=:search 
+// returns a list of acronyms, pagination using query parameters, response headers indicate if there are more results, 
+// and returns all acronyms that fuzzy match against: search
+
+app.get("/acronym", async (req, res) => {
+    if (req.query._id && req.query._id !== "") {
+        const result = await posts.find({ _id: req.query._id });
+        res.status(200).json(result);
+    } else if (req.query.search && req.query.search !== "") {
+        const result = await posts.find({ acronym: { $regex: req.query.search, $options: "i" } });
+        res.status(200).json(result);
+    } else {
+        const result = await posts.find();
+        res.status(200).json(result);
+    }
+})
+
 
 
 app.listen(port, async (err) => {
